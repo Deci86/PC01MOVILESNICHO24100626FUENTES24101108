4a5921132fb5f132e8819f6a1d6db55341562db8
Niove-F <niovelbv@gmail.com>
2026-05-30 11:38:02 -0500

    Pantalla planificador inicial

---
+++ app/src/main/java/com/example/pc01movilesnicho24100626fuentes24101108/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.pc01movilesnicho24100626fuentes24101108
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.pc01movilesnicho24100626fuentes24101108.presentation.pantallas.budgetPlanner
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background
+            ) {
+                budgetPlanner()
+            }
+        }
+    }
+}
